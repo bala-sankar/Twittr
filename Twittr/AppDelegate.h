fd095fa5a9cd5c03bb95be7d03d8286fafc40c2f
Balachandar Sankar <balas@yahoo-inc.com>
2017-01-30 10:10:46 -0800

    Initial Commit

---
+++ Twittr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twittr
+//
+//  Created by Balachandar Sankar on 1/30/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twittr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twittr
+//
+//  Created by Balachandar Sankar on 1/30/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
