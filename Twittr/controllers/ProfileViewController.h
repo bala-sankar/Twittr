2276b377acfcc9fc82d92bf5ee2854a19fdbf244
Balachandar Sankar <balas@yahoo-inc.com>
2017-02-01 18:36:49 -0800

    Tweet detail view controller design

---
+++ Twittr/controllers/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Twittr
+//
+//  Created by Balachandar Sankar on 2/1/17.
+//  Copyright © 2017 Balachandar Sankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
