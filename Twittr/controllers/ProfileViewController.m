16639c396b053958327ce1fa4952659a5cdb6b7e
Balachandar Sankar <balas@yahoo-inc.com>
2017-02-03 14:10:54 -0800

    Add user timeline tweets in profile page

--- Twittr/controllers/ProfileViewController.m
+++ Twittr/controllers/ProfileViewController.m
@@ -8,9 +8,11 @@
 
 #import "ProfileViewController.h"
 #import <UIImageView+AFNetworking.h>
+#import "TweetTableViewCell.h"
+#import "TwitterCLient.h"
+#import "NavigationManager.h"
 
-
-@interface ProfileViewController ()
+@interface ProfileViewController () <UITableViewDataSource, TweetTableViewCellDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *backgroudImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
@@ -21,6 +23,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *followingCount;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bacgroundImageViewTopConstraint;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) NSArray<TweetModel *> *tweetModels;
+@property (weak, nonatomic) IBOutlet UITableView *tweetListTableView;
 
 @end
 
@@ -28,6 +32,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tweetListTableView.dataSource = self;
+    self.tweetListTableView.estimatedRowHeight = 200;
+    
+    UINib *nib = [UINib nibWithNibName:@"TweetTableViewCell" bundle:nil];
+    [self.tweetListTableView registerNib:nib forCellReuseIdentifier:@"TweetTableViewCell"];
+    self.tweetListTableView.showsVerticalScrollIndicator = NO;
+    [self.view setNeedsUpdateConstraints];
+
     [self refreshData];
 }
 
@@ -101,7 +113,50 @@
     self.followingCount.text = [NSString stringWithFormat:@"%@ Following", self.user.friendsCount];
     self.locationLabel.text = self.user.location;
     
-    [self.view setNeedsUpdateConstraints];    
+    [self.view setNeedsUpdateConstraints];
+    [self fetchTweets];
 }
+
+- (void) fetchTweets {
+    NSString *URLString = @"1.1/statuses/user_timeline.json";
+    NSMutableDictionary *params = [NSMutableDictionary new];
+    [params setValue:self.user.screenName forKey:@"screen_name"];
+    [[TwitterClient sharedInstance] GET:URLString
+                             parameters:params
+                               progress:^(NSProgress * _Nonnull downloadProgress) {
+                                   NSLog(@"Request to fetch tweets in progress");
+                               }
+                                success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                                    NSLog(@"Retrieved tweets successfully");
+                                    self.tweetModels = [TweetModel convertTweets:responseObject];
+                                    [self.tweetListTableView reloadData];
+                                }
+                                failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                                    NSLog(@"Failed to retrieve tweets");
+                                }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.tweetModels.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TweetTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetTableViewCell" forIndexPath:indexPath];
+    cell.tweet = [self.tweetModels objectAtIndex:indexPath.row];
+    cell.delegate = self;
+    [cell refreshData];
+    return cell;
+}
+
+- (void)didTapTweet:(TweetTableViewCell *)tableViewCell Tweet:(TweetModel *)tweet {
+    [[NavigationManager sharedInstance] pushTweetDetailView:tweet];
+    
+}
+
+- (void)didTapImage:(TweetTableViewCell *)tableViewCell User:(UserModel *)user {
+    [[NavigationManager sharedInstance] pushProfileView:user];
+}
+
+
 
 @end
