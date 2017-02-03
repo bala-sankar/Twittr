565077a5b8ab92b9d95f8bbd8bebf816943cb9cb
Balachandar Sankar <balas@yahoo-inc.com>
2017-02-03 02:10:25 -0800

    Add pull to refresh to table

--- Twittr/controllers/TweetListViewController.m
+++ Twittr/controllers/TweetListViewController.m
@@ -16,6 +16,7 @@
 
 @property (weak, nonatomic) IBOutlet UITableView *tweetListTableView;
 @property (strong, nonatomic) NSArray *tweetModels;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -23,12 +24,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.refreshControl = [UIRefreshControl new];
+    self.refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"Pull to Refresh"];
+
     self.tweetListTableView.dataSource = self;
     self.tweetListTableView.estimatedRowHeight = 200;
     
     UINib *nib = [UINib nibWithNibName:@"TweetTableViewCell" bundle:nil];
     [self.tweetListTableView registerNib:nib forCellReuseIdentifier:@"TweetTableViewCell"];
     self.tweetListTableView.showsVerticalScrollIndicator = NO;
+    [self.view setNeedsUpdateConstraints];
+
+    [self.refreshControl addTarget:self action:@selector(onRefresh) forControlEvents:UIControlEventValueChanged];
+    [self.tweetListTableView insertSubview:self.refreshControl atIndex:0];
+
     [self fetchTweets];
 }
 
@@ -73,6 +82,12 @@
 
 - (void)didTapImage:(TweetTableViewCell *)tableViewCell User:(UserModel *)user {
      [[NavigationManager sharedInstance] pushProfileView:user];
+}
+
+- (void)onRefresh {
+    [self.refreshControl beginRefreshing];
+    [self fetchTweets];
+    [self.refreshControl endRefreshing];
 }
 
 @end
